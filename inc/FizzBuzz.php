d5d65778d771bc054f74191a25d47c42720ac0cd
ettorestark <ettoreastark@gmail.com>
2021-07-12 22:55:06 -0500

    Initial commit

---
+++ inc/FizzBuzz.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Inc;
+
+class FizzBuzz
+{
+	public function changeMe()
+	{
+		return true;
+	}
+
+	public function getData()
+	{
+		$numbers = [];
+		$numbers[1] = 1;
+		$numbers[3] = 'Fizz';
+		$numbers[5] = 'Buzz';
+		return $numbers;
+	}
+}
---
+++ tests/FizzBuzzTest.php
@@ -0,0 +1,33 @@
+<?php
+
+use Inc\FizzBuzz;
+
+class FizzBuzzTest extends PHPUnit\Framework\TestCase 
+{
+	private $fizzBuzz;
+
+	protected function setUp(): void
+	{
+		parent::setUp();
+		$this->fizzBuzz = new FizzBuzz();
+	}
+
+	public function testFirstNumberIsOne()	
+	{
+		$result = $this->fizzBuzz->getData()[1];
+		$this->assertEquals(1, $result);
+	}
+
+	public function testThirdNumberIsFizz()
+	{
+		$result = $this->fizzBuzz->getData()[3];
+		$this->assertEquals('Fizz', $result);
+	}
+
+	public function testFithNumberIsBuzz()
+	{
+		$result = $this->fizzBuzz->getData()[5];
+		$this->assertEquals('Buzz', $result);
+
+	}
+}
